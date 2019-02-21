5a1a1f6106307a18a2385dc7dd1deffcad29d4db
thessler27 <hessler.thomas93@gmail.com>
2019-02-21 10:23:32 -0500

    start core and update cli to alpha

---
+++ lib/components/globals/View/index.ts
@@ -0,0 +1,9 @@
+import { OnceComponent } from '@once/core';
+
+export default class View extends OnceComponent {
+
+	constructor (props) {
+		super(props);
+	}
+
+}
---
+++ lib/core/Component.ts
@@ -0,0 +1,29 @@
+/**
+Once Framework: The build once, use everywhere framework
+core/OnceComponent
+
+@flow
+
+All Once Components `extend` OnceComponent
+
+*/
+
+export default class Component {
+
+	constructor () {}
+
+	/**
+	A style template literal to be passed onto
+	css files (Angular) or to Styled Components 
+	(React)
+	@static
+	*/
+	static style = ``;
+
+	/**
+	Rendering a Component with Once.
+	@method
+	*/
+	public render () {}
+
+}
